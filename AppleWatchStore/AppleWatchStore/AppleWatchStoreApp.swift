417341438bb3fcfbfedef0c4e874d1413b2c2833
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-06-10 19:01:47 +0200

    introduce assets

---
+++ AppleWatchStore/AppleWatchStore/AppleWatchStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchStoreApp.swift
+//  AppleWatchStore
+//
+//  Created by Arthur Nsereko Kahwa on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatchStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
