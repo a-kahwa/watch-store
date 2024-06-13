88eab00b27c822730db1bef99ffdbf4b071841c5
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-06-13 18:24:19 +0200

    grid product item

---
+++ AppleWatchStore/AppleWatchStore/views/home/GridProductItem.swift
@@ -0,0 +1,66 @@
+//
+//  GridProductItem.swift
+//  AppleWatchStore
+//
+//  Created by Arthur Nsereko Kahwa on 6/13/24.
+//
+
+import SwiftUI
+
+struct GridProductItem: View {
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            VStack {
+                ZStack {
+                    Image(.sportBandProductRedL)
+                        .resizable()
+                        .frame(width: 250, height: 250)
+                    
+                    Image(.aluminumMidnightL)
+                        .resizable()
+                        .frame(width: 250, height: 250)
+                }
+                
+                VStack {
+                    Text("€").ultraLight() + Text("888").heavy()
+                    
+                    Text("Midnight Aluminium Case")
+                        .condensed(.bold, size: 16)
+                    
+                    Text("Braided Solo Loop")
+                        .condensed(.light, size: 16)
+                }
+                .foregroundStyle(.primary)
+                .padding(.bottom, 4)
+                
+                HStack {
+                    ForEach(0..<4) { _ in
+                        Circle()
+                            .frame(width: 20, height: 20)
+                    }
+                }
+                .padding(.bottom)
+            }
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .background(
+            RoundedRectangle(cornerRadius: 12)
+                .fill(Constants.gradient)
+        )
+        .overlay(alignment: .topTrailing) {
+            heart
+        }
+    }
+    
+    var heart: some View {
+        Image(systemName: "heart")
+            .symbolVariant(.none)
+            .font(.system(size: 24))
+            .padding(.trailing, 20)
+            .padding(.top, 10)
+    }
+}
+
+#Preview {
+    GridProductItem()
+}
