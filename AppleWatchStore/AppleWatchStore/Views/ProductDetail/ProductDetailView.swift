99a8ff6d3b48a8b3a00118995799340c6f106f55
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-06-21 12:43:02 +0200

    Product detail

--- AppleWatchStore/AppleWatchStore/Views/ProductDetail/ProductDetailView.swift
+++ AppleWatchStore/AppleWatchStore/Views/ProductDetail/ProductDetailView.swift
@@ -14,7 +14,6 @@ struct ProductDetailView: View {
                 header
                 
                 colors
-                    .padding(.horizontal)
                 
                 VStack(spacing: 48) {
                     ProductDetailDescriptionView()
@@ -27,7 +26,16 @@ struct ProductDetailView: View {
                     
                     AppleCareView()
                 }
+                .padding(.horizontal)
             }
+        }
+        .safeAreaInset(edge: .bottom) {
+            ZStack {
+                addToCart
+            }
+            .frame(height: 80)
+            .padding(.vertical, 8)
+            .background(.ultraThinMaterial)
         }
     }
 
@@ -55,17 +63,31 @@ struct ProductDetailView: View {
     var colors: some View {
         VStack {
             SectionHeader(title: "Brand Colors")
+                .padding(.horizontal)
             
             HStack {
                 ForEach(0 ..< 3) { item in
                     Circle()
                         .fill(.gray)
-                    .frame(width: 32, height: 32)
+                        .frame(width: 32, height: 32)
                 }
                 
                 Spacer()
             }
-        }    }
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
+    }
+    
+    var addToCart: some View {
+        Button(action: {}) {
+            Text("Add To Cart")
+                .condensedLowercased(.medium, size: 24)
+                .foregroundStyle(.white)
+        }
+        .buttonStyle(.customBorderedBlack)
+        .padding(.horizontal)
+    }
 }
 
 #Preview {
--- AppleWatchStore/AppleWatchStore/Views/ProductDetail/SupportingViews/ProductDetailDescriptionView.swift
+++ AppleWatchStore/AppleWatchStore/Views/ProductDetail/SupportingViews/ProductDetailDescriptionView.swift
@@ -39,9 +39,11 @@ struct ProductDetailDescriptionView: View {
                     }
                 }
                 .padding(.bottom, 4)
+                .padding(.horizontal)
                 
                 Text("The aluminum case is lightweight and made from 100 percent recycled aerospace-grade alloy. \n\nThe Nike Sport Band is made from a durable high-performance fluoroelastomer with compression-molded perforations for breathability.")
                     .condensed(.light, size: 20)
+                    .padding(.horizontal)
             }
             
             Spacer()
