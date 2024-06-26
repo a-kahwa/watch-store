a0c7dbc0513db5b9f16396afbbc9754bb3d0e61f
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-06-26 18:26:16 +0200

    featured products and card view

--- AppleWatchStore/AppleWatchStore/Utilities/Constants.swift
+++ AppleWatchStore/AppleWatchStore/Utilities/Constants.swift
@@ -31,4 +31,11 @@ enum Constants {
             .padding(.trailing, 20)
             .padding(.top, 10)
     }
+    
+    static let featuredProducts: [FeaturedProduct] = [
+        FeaturedProduct(image: "watch-card-40-ultra", title: "Apple Watch Ultra", description: "Ready for adventure"),
+        FeaturedProduct(image: "watch-card-40-se", title: "Apple Watch SE", description: "Capable and affordable"),
+        FeaturedProduct(image: "watch-card-40-s8", title: "Apple Watch Series 8", description: "Advanced health features"),
+        FeaturedProduct(image: "watch-card-40-hermes", title: "Apple Watch Hermes", description: "Fashion Forward")
+    ]
 }
