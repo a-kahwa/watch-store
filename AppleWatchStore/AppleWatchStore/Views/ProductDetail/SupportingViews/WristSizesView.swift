1c85c8522d94a2b69243f55e97f91e32de1c3f34
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-06-27 13:56:23 +0200

    integrate wrist size view

--- AppleWatchStore/AppleWatchStore/Views/ProductDetail/SupportingViews/WristSizesView.swift
+++ AppleWatchStore/AppleWatchStore/Views/ProductDetail/SupportingViews/WristSizesView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct WristSizesView: View {
+    let product: Product
+    
+    var sortedSizes: [WristSize] {
+        get {
+            product.wristSizes.sorted(by: { $0.order < $1.order })
+        }
+        set {
+            product.wristSizes = newValue
+        }
+    }
+    
     var body: some View {
         VStack {
             SectionHeader(title: "Wrist Sizes")
                 .padding(.bottom)
             
-            ForEach(0 ..< 3) { item in
+            ForEach(sortedSizes) { size in
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                    WristItemView()
+                    WristItemView(item: size)
                         .contentShape(RoundedRectangle(cornerRadius: 8))
                         .overlay(
                             RoundedRectangle(cornerRadius: 8)
@@ -29,6 +40,6 @@ struct WristSizesView: View {
     }
 }
 
-#Preview {
-    WristSizesView()
-}
+//#Preview {
+//    WristSizesView()
+//}
