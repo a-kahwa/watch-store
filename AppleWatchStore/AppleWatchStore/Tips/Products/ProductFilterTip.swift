9c8031bdec20f089d90d5bb55facaed12493b83d
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-07-08 18:18:40 +0200

    Produdct layout tips

---
+++ AppleWatchStore/AppleWatchStore/Tips/Products/ProductFilterTip.swift
@@ -0,0 +1,9 @@
+//
+//  ProductFilterTip.swift
+//  AppleWatchStore
+//
+//  Created by Arthur Nsereko Kahwa on 7/8/24.
+//
+
+import Foundation
+import TipKit
--- AppleWatchStore/AppleWatchStore/Tips/Products/ProductLayoutTip.swift
+++ AppleWatchStore/AppleWatchStore/Tips/Products/ProductLayoutTip.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import TipKit
+
+struct ProductLayoutTip: Tip {
+    var title: Text {
+        Text("Switching Layouts")
+    }
+    
+    var message: Text {
+        Text("You can choose to switch layouts by toggling the button")
+    }
+    
+    var image: Image? {
+        Image(systemName: "rectangle.grid.1x2.fill")
+    }
+    
+    var actions: [Action] {
+        [
+            Tip.Action(id: "next-action",
+                       title: "Next Tip")
+        ]
+    }
+}
