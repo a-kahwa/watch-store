32778b59131323c6b75dcfd79a264a45878a3184
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-07-08 18:40:14 +0200

    Produdct filter tips setup

--- AppleWatchStore/AppleWatchStore/Tips/Products/ProductFilterTip.swift
+++ AppleWatchStore/AppleWatchStore/Tips/Products/ProductFilterTip.swift
@@ -7,3 +7,35 @@
 
 import Foundation
 import TipKit
+
+struct ProductFilterTip: Tip {
+    static let productSteps: Event = Event<Tips.EmptyDonation>(id: "tip.event.product-steps")
+    
+    public var title: Text {
+        Text("Product Filters")
+    }
+    
+    public var message: Text {
+        Text("You can filter for those products that interest you on this page.")
+    }
+    
+    public var image: Image? {
+        Image(systemName: "slider.horizontal.3")
+    }
+    
+    public var actions: [Action] {
+        [
+            Action(id: "action.title.dismiss",
+                   title: "Dismiss")
+        ]
+    }
+    
+    public var rules: [Rule] {
+        #Rule(Self.productSteps) { $0.donations.count == 1 }
+    }
+    
+    public var options: [TipOption] {
+        [Tips.MaxDisplayCount(1)]
+    }
+    
+}
